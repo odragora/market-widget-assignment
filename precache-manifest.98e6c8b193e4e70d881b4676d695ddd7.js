3043ab64d7f3024e5f7b9d26e26760b1ba18f739
okirichenko <odragora@gmail.com>
2020-05-04 12:39:58 +0300

    Updates

---
+++ precache-manifest.98e6c8b193e4e70d881b4676d695ddd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edab052102572bad69eb33bae21e663",
+    "url": "/market-widget-assignment/index.html"
+  },
+  {
+    "revision": "ffe3058673228563c464",
+    "url": "/market-widget-assignment/static/css/main.938819dd.chunk.css"
+  },
+  {
+    "revision": "a5714571c4855827433c",
+    "url": "/market-widget-assignment/static/js/2.e53f5541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/market-widget-assignment/static/js/2.e53f5541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe3058673228563c464",
+    "url": "/market-widget-assignment/static/js/main.26c94aa2.chunk.js"
+  },
+  {
+    "revision": "affe72fde57f0cc92705",
+    "url": "/market-widget-assignment/static/js/runtime-main.275edfd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/market-widget-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
